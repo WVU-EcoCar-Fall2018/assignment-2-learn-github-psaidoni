aff81ad497056861cba2f2121d78fabed539cae6
Rob <rsphillips@mix.wvu.edu>
2018-09-10 11:44:26 -0400

    First Commit

---
+++ hellomake.c
@@ -0,0 +1,7 @@
+Copyright 2018 <Pamela Saidoni>
+#include <hellomake.h>
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+  return(0);
+}
---
+++ hellomake.h
@@ -0,0 +1,5 @@
+Copyright 2018 <Pamela Saidoni>
+#ifndef DOWNLOADS_HELLOMAKE_H_
+#define DOWNLOADS_HELLOMAKE_H_
+void myPrintHelloMake(void);
+#endif  // DOWNLOADS_HELLOMAKE_H_
