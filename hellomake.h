cf5c32414beca434c9b511f4686de721c4e921b8
pamela <pamela@oem-VirtualBox>
2018-09-12 21:27:53 -0400

    testing my virtual machine hellofiles

--- hellomake.h
+++ hellomake.h
@@ -1,5 +1,5 @@
-Copyright 2018 <Pamela Saidoni>
-#ifndef DOWNLOADS_HELLOMAKE_H_
-#define DOWNLOADS_HELLOMAKE_H_
+"Copyright [2018] <Pamela Saidoni>"  [legal/copyright]
+#ifndef  _HOME_PAMELA_DOCUMENTS_HELLO_HELLOMAKE_H_
+#define  _HOME_PAMELA_DOCUMENTS_HELLO_HELLOMAKE_H_
 void myPrintHelloMake(void);
-#endif  // DOWNLOADS_HELLOMAKE_H_
+#endif  //  _HOME_PAMELA_DOCUMENTS_HELLO_HELLOMAKE_H_
